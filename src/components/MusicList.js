de21a92397aa1064af2b7b81ba5297ec12752cff
Justin Mendiguarin <justin.mendiguarin@gmail.com>
2018-02-18 18:21:10 -0800

    test

---
+++ src/components/MusicList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class MusicList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Music List</Text>
+      </View>
+    );
+  }
+}
+
+export default MusicList;
