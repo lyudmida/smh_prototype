bcf4a56886d94ee807f7c28fafef37e084dc4304
B E A <joseivansveran@BEAs-MacBook-Pro.local>
2018-02-18 16:05:31 -0800

    commit 021818.1605

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,26 @@
 // Import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // Make a component
 const Header = () => {
-  return <Text>Hallo hallo</Text>;
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Hallo hallo</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#BEABEA'
+  },
+
+  textStyle: {
+    fontSize: 40
+  }
 };
 
 // Make the component available to other parts of the app
