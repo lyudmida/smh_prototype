8fe53c8454a1261ad55bdcae82a1a3bc1cd5105b
B E A <joseivansveran@BEAs-MacBook-Pro.local>
2018-02-18 17:12:29 -0800

    commit 021818.1712

---
+++ src/components/MusicList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MusicList = () => {
+  return (
+    <View>
+      <Text>Music List</Text>
+    </View>
+  );
+};
+
+export default MusicList;
