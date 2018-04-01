10b18f48798c603086024425222fbf1c4a597293
B E A <joseivansveran@BEAs-MacBook-Pro.local>
2018-04-01 13:45:01 -0700

    040118.1344

--- src/Front.js
+++ src/Front.js
@@ -1,33 +1,12 @@
 import React, { Component } from 'react';
 import { Image, ListView, Text, TouchableOpacity, View } from 'react-native';
 import { Card, Header } from 'react-native-elements';
-// import Header from './components/Header';
-// import Card from './components/Card';
-// import CardSection from './components/CardSection';
-/*
-const styles = {
-  cardStyle: {
-    width: 365,
-    height: 278,
-    borderBottomWidth: 1,
-    padding: 3,
-    backgroundColor: '#000',
-    justifyContent: 'center',
-    flexDirection: 'column',
-    alignItems: 'center',
-    borderColor: '#ddd',
-    position: 'relative',
-    opacity: 0.99
-  }
-};
-*/
 
 class Front extends Component {
   static navigationOptions = {
     headerTitle: <Image source={require('./icons/heartnote.png')}
             style={{width: 44, height: 44}} />,
   };
-
 
   render() {
     return (
@@ -71,42 +50,7 @@ class Front extends Component {
     )
   }
 }
-/*      <View>
-        <Header headerText={'SMH'} />
-        <Card>
-          <CardSection>
-            <View style={styles.thumbnailContainerStyle}>
-              <Image
-                style={styles.thumbnailStyle}
-                source={require('./icons/heartFront.png')}
-              >
-                <View style={styles.contentStyle}>
-                  <Text style={styles.textStyle}>Health</Text>
-                </View>
-              </Image>
-            </View>
-          </CardSection>
-        </Card>
 
-        <Card>
-          <CardSection>
-            <View style={styles.thumbnailContainerStyle}>
-              <Image
-                style={styles.thumbnailStyle}
-                source={require('./icons/musicFront.png')}
-              >
-                <View style={styles.contentStyle}>
-                  <Text style={styles.textStyle}>Music</Text>
-                </View>
-              </Image>
-            </View>
-          </CardSection>
-        </Card>
-      </View>
-    );
-  }
-}
-*/
 const styles = {
   cardStyle: {
     backgroundColor: '#000',
@@ -134,24 +78,7 @@ const styles = {
   contentStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
-  },
-/*
-
-
-thumbnailStyle: {
-  height: 250,
-  width: 250
-},
-
-thumbnailContainerStyle: {
-  flex: 1,
-  flexDirection: 'row',
-  justifyContent: 'center',
-  alignItems: 'center',
-  marginLeft: 10,
-  marginRight: 10
-},
-*/
+  }
 };
 
 export default Front;
